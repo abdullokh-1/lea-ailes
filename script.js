72e19855455ec26c586d103c7cee21273fd20bf4
abdullokh-1 <a87835954@gmail.com>
2025-03-15 18:12:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+   let qiy = {
+    price:45000
+}
+let sne = {
+    price:79000
+}
+let clas = {
+    price:34000
+}
+
+let lon = {
+    price:45000
+}
+let b = {
+    price:62000
+}
+let last = {
+    price:53000
+}
+let schoo = {
+    price:36000 
+}
+let com = {
+    price:20000
+}
+let fre = {
+    price:43000
+}
+
+let fre2 = {
+    price:79000
+}
+let fre4 = {
+    price:149000
+}
+let ach = {
+    price:43000
+}
+
+let qiyqiriq = document.getElementById("qiyqiriq")
+let shashlik = document.getElementById("shashlik")
+
+qiyqiriq.onclick = function(){
+    let soni = prompt(`Siz qiyqiriq setini  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta qiyqiriq seti ${soni * qiy.price} so'm`)
+   }
+
+   snek.onclick = function(){
+    let soni = prompt(`Siz snekni tanladiz nechta olmoqchisiz`)
+    alert(`${soni} ta snek ${soni * sne.price} so'm`)                                         
+   }
+
+
+   klassic.onclick = function(){
+    let soni = prompt(`Siz classic setni tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta klassik  set ${soni * clas.price} so'm`)                                         
+   }
+
+   longer.onclick = function(){
+    let soni = prompt(`Siz  longer ring set tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta longer ring set ${soni * lon.price} so'm`)                                         
+   }
+
+   big.onclick = function(){
+    let soni = prompt(`Siz  big set tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta big set ${soni * b.price} so'm`)                                         
+   }
+   laster.onclick = function(){
+    let soni = prompt(`Siz  laster set tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta laster set ${soni * last.price} so'm`)                                         
+   }
+
+   school.onclick = function(){
+    let soni = prompt(`Siz  school set tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta school set ${soni * schoo.price} so'm`)                                         
+   }
+   combo.onclick = function(){
+    let soni = prompt(`Siz combo set big  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta combo set big ${soni * com.price} so'm`)                                         
+   }
+   freand.onclick = function(){
+    let soni = prompt(`Siz freand 1x  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta freand 1x ${soni * fre.price} so'm`)                                         
+   }
+
+   freands.onclick = function(){
+    let soni = prompt(`Siz freand 2x  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta freand 2x ${soni * fre2.price} so'm`)                                         
+   }
+   dost.onclick = function(){
+    let soni = prompt(`Siz freand 4x  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta freand 4x ${soni * fre4.price} so'm`)                                         
+   }
+   achiq.onclick = function(){
+    let soni = prompt(`Siz freand 1x  tanladingiz nechta olmoqchisiz`)
+    alert(`${soni} ta  achiq freand 1x ${soni * ach.price} so'm`)                                         
+   }
+
+
+
+
+
+
